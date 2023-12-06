69d33ba908aef913431a5f013ff128a88bccaa08
Leonardo Matteucci <117661364+bananadado@users.noreply.github.com>
2023-12-06 05:25:38 +0000

    Day 6

--- 2023/Day06.py
+++ 2023/Day06.py
@@ -1,3 +1,15 @@
+def ways(inp):
+    prod = 1
+    for i in range(len(inp[0])):
+        total = 0
+        for j in range(inp[0][i]):
+            if j * (inp[0][i] - j) > inp[1][i]:
+                total += 1
+        prod *= total
+    return prod
+
 
 with open("input.txt", "r") as f:
-    inp = [line.strip() for line in f.readlines()]
+    lines = f.readlines()
+    print(f"Part 1: {ways([[int(x) for x in line.strip().split(':')[1].split()] for line in lines])}")
+    print(f"Part 2: {ways([[int(''.join(line.strip().split(':')[1].split()))] for line in lines])}")
