8c7a3782fbbfa113ddee85375a892a913ddfdc0e
Leonardo Matteucci <117661364+bananadado@users.noreply.github.com>
2023-12-11 06:27:53 +0000

    Day 11

--- 2023/Day10.py
+++ 2023/Day10.py
@@ -2,16 +2,13 @@
 # Part 2 essentially just searches outwards as in Day 18 of 2022
 # A more optimal solution would be to consider a line (e.g. ..|...|..|.|..),
 # where it is evident that the 2nd and 4th gaps would be inside the loop.
-# I'm still in the process of neatening this code up so gimme a minute lol
-
 from collections import deque
 
 with open("input.txt", "r") as f:
     inp = [[x for x in line.strip()] for line in f.readlines()]
 
 explored = [[False] * len(inp[0]) for _ in range(len(inp))]
-inLoop = [[0] * len(inp[0]) for _ in range(len(inp))]
-q = deque()
+q = deque()  # global queue that I will use for everything
 
 # Find S and replace it
 # (U, L, D, R)
@@ -27,202 +24,151 @@ for i in range(len(inp)):
 
 # Part 1
 def checkNorth1(row, col):
-    if row > 0 and not explored[row - 1][col]:
-        if inp[row - 1][col] in '7F|':
-            explored[row - 1][col] = True
-            q.append((row - 1, col, dist + 1))
-
+    if not explored[row - 1][col]:
+        explored[row - 1][col] = True
+        q.append((row - 1, col, dist + 1))
 
 def checkSouth1(row, col):
-    if row < len(inp) - 1 and not explored[row + 1][col]:
-        if inp[row + 1][col] in 'LJ|':
-            explored[row + 1][col] = True
-            q.append((row + 1, col, dist + 1))
-
+    if not explored[row + 1][col]:
+        explored[row + 1][col] = True
+        q.append((row + 1, col, dist + 1))
 
 def checkWest1(row, col):
-    if col > 0 and not explored[row][col - 1]:
-        if inp[row][col - 1] in 'LF-':
-            explored[row][col - 1] = True
-            q.append((row, col - 1, dist + 1))
-
+    if not explored[row][col - 1]:
+        explored[row][col - 1] = True
+        q.append((row, col - 1, dist + 1))
 
 def checkEast1(row, col):
-    if col < len(inp[0]) - 1 and not explored[row][col + 1]:
-        if inp[row][col + 1] in '7J-':
-            explored[row][col + 1] = True
-            q.append((row, col + 1, dist + 1))
+    if not explored[row][col + 1]:
+        explored[row][col + 1] = True
+        q.append((row, col + 1, dist + 1))
 
 
 pipeMap = {v: k for k, v in pipeMap.items()}
 high = 0
 while q:
-    row, col, dist = q.popleft()
+    r, c, dist = q.popleft()
     high = max(dist, high)
 
-    n, w, s, e = pipeMap[inp[row][col]]
+    n, w, s, e = pipeMap[inp[r][c]]
     if n:
-        checkNorth1(row, col)
+        checkNorth1(r, c)
     if w:
-        checkWest1(row, col)
+        checkWest1(r, c)
     if s:
-        checkSouth1(row, col)
+        checkSouth1(r, c)
     if e:
-        checkEast1(row, col)
+        checkEast1(r, c)
 
 print(f"Part 1: {high}")
 
-
 # Part 2
-def checkNorth2(row, col, dir, seen):
-    if row > 0 and (row - 1, col) not in seen:
-        if inp[row - 1][col] in ['7', 'F', '|']:
-            seen.add((row - 1, col))
-            if inp[row][col] == 'L' and dir == 'U':
-                q.append((row - 1, col, 'R'))
-            elif inp[row][col] == 'L' and dir == 'D':
-                q.append((row - 1, col, 'L'))
-            elif inp[row][col] == 'J' and dir == 'U':
-                q.append((row - 1, col, 'L'))
-            elif inp[row][col] == 'J' and dir == 'D':
-                q.append((row - 1, col, 'R'))
-            else:
-                q.append((row - 1, col, dir))
+north = {'LU': 'R', 'LD': 'L', 'JU': 'L', 'JD': 'R'}
+def checkNorth2(row, col, d, seen):
+    if (row - 1, col) not in seen:
+        seen.add((row - 1, col))
+        q.append((row - 1, col, north.get(inp[row][col] + d, d)))
 
+south = {'7U': 'R', '7D': 'L', 'FU': 'L', 'FD': 'R'}
+def checkSouth2(row, col, d, seen):
+    if (row + 1, col) not in seen:
+        seen.add((row + 1, col))
+        q.append((row + 1, col, south.get(inp[row][col] + d, d)))
 
-def checkSouth2(row, col, dir, seen):
-    if row < len(inp) - 1 and (row + 1, col) not in seen:
-        if inp[row + 1][col] in ['L', 'J', '|']:
-            seen.add((row + 1, col))
-            if inp[row][col] == '7' and dir == 'U':
-                q.append((row + 1, col, 'R'))
-            elif inp[row][col] == '7' and dir == 'D':
-                q.append((row + 1, col, 'L'))
-            elif inp[row][col] == 'F' and dir == 'U':
-                q.append((row + 1, col, 'L'))
-            elif inp[row][col] == 'F' and dir == 'D':
-                q.append((row + 1, col, 'R'))
-            else:
-                q.append((row + 1, col, dir))
+west = {'JR': 'D', 'JL': 'U', '7R': 'U', '7L': 'D'}
+def checkWest2(row, col, d, seen):
+    if (row, col - 1) not in seen:
+        seen.add((row, col - 1))
+        q.append((row, col - 1, west.get(inp[row][col] + d, d)))
 
-
-def checkWest2(row, col, dir, seen):
-    if col > 0 and (row, col - 1) not in seen:
-        if inp[row][col - 1] in ['-', 'L', 'F']:
-            seen.add((row, col - 1))
-            if inp[row][col] == 'J' and dir == 'R':
-                q.append((row, col - 1, 'D'))
-            elif inp[row][col] == 'J' and dir == 'L':
-                q.append((row, col - 1, 'U'))
-            elif inp[row][col] == '7' and dir == 'R':
-                q.append((row, col - 1, 'U'))
-            elif inp[row][col] == '7' and dir == 'L':
-                q.append((row, col - 1, 'D'))
-            else:
-                q.append((row, col - 1, dir))
-
-
-def checkEast2(row, col, dir, seen):
-    if col < len(inp[0]) - 1 and (row, col + 1) not in seen:
-        if inp[row][col + 1] in ['-', 'J', '7']:
-            seen.add((row, col + 1))
-            if inp[row][col] == 'L' and dir == 'R':
-                q.append((row, col + 1, 'U'))
-            elif inp[row][col] == 'L' and dir == 'L':
-                q.append((row, col + 1, 'D'))
-            elif inp[row][col] == 'F' and dir == 'R':
-                q.append((row, col + 1, 'D'))
-            elif inp[row][col] == 'F' and dir == 'L':
-                q.append((row, col + 1, 'U'))
-            else:
-                q.append((row, col + 1, dir))
+east = {'LR': 'U', 'LL': 'D', 'FR': 'D', 'FL': 'U'}
+def checkEast2(row, col, d, seen):
+    if (row, col + 1) not in seen:
+        seen.add((row, col + 1))
+        q.append((row, col + 1, east.get(inp[row][col] + d, d)))
 
 
 for i in range(len(explored)):
     for j in range(len(explored[0])):
-        if explored[i][j] or inp[i][j] in ['O', 'I']:
+        if explored[i][j] or inp[i][j] in 'OI':
             continue
-        loopSet = set()
-        loopSet.add((i, j))
+        loopSet = {(i, j)}
         q = deque()
         q.append((i, j))
 
+        # Find a set of connected non-pipes
         outside = False
         while q:
-            row, col = q.popleft()
-            if not outside:
-                outside = row == 0 or row == len(inp) - 1 or col == 0 or col == len(inp[0]) - 1
+            r, c = q.popleft()
+            outside = outside or r == 0 or r == len(inp) - 1 or c == 0 or c == len(inp[0]) - 1
 
-            if row > 0 and (row - 1, col) not in loopSet and not explored[row - 1][col]:
-                q.append((row - 1, col))
-                loopSet.add((row - 1, col))
-            if row < len(inp) - 1 and (row + 1, col) not in loopSet and not explored[row + 1][col]:
-                q.append((row + 1, col))
-                loopSet.add((row + 1, col))
-            if col < len(inp[0]) - 1 and (row, col + 1) not in loopSet and not explored[row][col + 1]:
-                q.append((row, col + 1))
-                loopSet.add((row, col + 1))
-            if col > 0 and (row, col - 1) not in loopSet and not explored[row][col - 1]:
-                q.append((row, col - 1))
-                loopSet.add((row, col - 1))
+            if r > 0 and (r - 1, c) not in loopSet and not explored[r - 1][c]:
+                q.append((r - 1, c))
+                loopSet.add((r - 1, c))
+            if r < len(inp) - 1 and (r + 1, c) not in loopSet and not explored[r + 1][c]:
+                q.append((r + 1, c))
+                loopSet.add((r + 1, c))
+            if c < len(inp[0]) - 1 and (r, c + 1) not in loopSet and not explored[r][c + 1]:
+                q.append((r, c + 1))
+                loopSet.add((r, c + 1))
+            if c > 0 and (r, c - 1) not in loopSet and not explored[r][c - 1]:
+                q.append((r, c - 1))
+                loopSet.add((r, c - 1))
 
-        # Try to find an escape
+        # Try to find an escape around the pipes
         if not outside:
-            r, c = list(loopSet)[0]
-            q.append((r, c, ''))
+            rt, ct = list(loopSet)[0]
+            q.append((rt, ct, ''))
             seen = set()
             while q:
-                row, col, dir = q.popleft()
-                seen.add((row, col))
+                r, c, d = q.popleft()
+                seen.add((r, c))
 
-                if inp[row][col] == 'I':
+                if inp[r][c] == 'I':
                     break
 
-                outside = not explored[row][col] and (row == 0 or row == len(inp) - 1 or col == 0 or col == len(inp[0]) - 1) \
-                          or inp[row][col] == 'L' and col > 0 and dir in 'LD' and inp[row][col - 1] == 'O' \
-                          or inp[row][col] == 'L' and row < len(inp) - 1 and dir in 'LD' and inp[row + 1][col] == 'O' \
-                          or inp[row][col] == 'J' and col < len(inp) - 1 and dir in 'RD' and inp[row][col + 1] == 'O' \
-                          or inp[row][col] == 'J' and row < len(inp) - 1 and dir in 'RD' and inp[row + 1][col] == 'O' \
-                          or inp[row][col] == '7' and col < len(inp) - 1 and dir in 'RU' and inp[row][col + 1] == 'O' \
-                          or inp[row][col] == '7' and row > 0 and dir in 'RU' and inp[row - 1][col] == 'O' \
-                          or inp[row][col] == 'F' and col > 0 and dir in 'LU' and inp[row][col - 1] == 'O' \
-                          or inp[row][col] == 'F' and row > 0 and dir in 'LU' and inp[row - 1][col] == 'O'
+                outside = not explored[r][c] and (r == 0 or r == len(inp) - 1 or c == 0 or c == len(inp[0]) - 1) \
+                          or inp[r][c] == 'L' and c > 0 and d in 'LD' and inp[r][c - 1] == 'O' \
+                          or inp[r][c] == 'L' and r < len(inp) - 1 and d in 'LD' and inp[r + 1][c] == 'O' \
+                          or inp[r][c] == 'J' and c < len(inp) - 1 and d in 'RD' and inp[r][c + 1] == 'O' \
+                          or inp[r][c] == 'J' and r < len(inp) - 1 and d in 'RD' and inp[r + 1][c] == 'O' \
+                          or inp[r][c] == '7' and c < len(inp) - 1 and d in 'RU' and inp[r][c + 1] == 'O' \
+                          or inp[r][c] == '7' and r > 0 and d in 'RU' and inp[r - 1][c] == 'O' \
+                          or inp[r][c] == 'F' and c > 0 and d in 'LU' and inp[r][c - 1] == 'O' \
+                          or inp[r][c] == 'F' and r > 0 and d in 'LU' and inp[r - 1][c] == 'O'
                 if outside:
                     break
 
-                if explored[row][col]:
-                    n, w, s, e = pipeMap.get(inp[row][col])
+                if explored[r][c]:
+                    n, w, s, e = pipeMap.get(inp[r][c])
                     if n:
-                        checkNorth2(row, col, dir, seen)
+                        checkNorth2(r, c, d, seen)
                     if w:
-                        checkWest2(row, col, dir, seen)
+                        checkWest2(r, c, d, seen)
                     if s:
-                        checkSouth2(row, col, dir, seen)
+                        checkSouth2(r, c, d, seen)
                     if e:
-                        checkEast2(row, col, dir, seen)
+                        checkEast2(r, c, d, seen)
                 else:
-                    if (row + 1, col) not in seen and (row + 1, col) not in loopSet:
-                        seen.add((row + 1, col))
-                        q.append((row + 1, col, 'U'))
-                    if (row - 1, col) not in seen and (row - 1, col) not in loopSet:
-                        seen.add((row - 1, col))
-                        q.append((row - 1, col, 'D'))
-                    if (row, col - 1) not in seen and (row, col - 1) not in loopSet:
-                        seen.add((row, col - 1))
-                        q.append((row, col - 1, 'R'))
-                    if (row, col + 1) not in seen and (row, col + 1) not in loopSet:
-                        seen.add((row, col - 1))
-                        q.append((row, col - 1, 'L'))
+                    if (r + 1, c) not in seen and (r + 1, c) not in loopSet:
+                        seen.add((r + 1, c))
+                        q.append((r + 1, c, 'U'))
+                    if (r - 1, c) not in seen and (r - 1, c) not in loopSet:
+                        seen.add((r - 1, c))
+                        q.append((r - 1, c, 'D'))
+                    if (r, c - 1) not in seen and (r, c - 1) not in loopSet:
+                        seen.add((r, c - 1))
+                        q.append((r, c - 1, 'R'))
+                    if (r, c + 1) not in seen and (r, c + 1) not in loopSet:
+                        seen.add((r, c - 1))
+                        q.append((r, c - 1, 'L'))
 
-        if outside:
-            for row, col in loopSet:
-                inp[row][col] = 'O'
-        else:
-            for row, col in loopSet:
-                inp[row][col] = 'I'
+        mark = 'O' if outside else 'I'
+        for r, c in loopSet:
+            inp[r][c] = mark
 
 total = 0
 for l in inp:
-    #print("".join(l)) # print the grid
+    # print("".join(l)) # print the grid
     total += sum(1 for x in l if x == 'I')
 print(f"Part 2: {total}")
--- 2023/Day11.py
+++ 2023/Day11.py
@@ -1,3 +1,32 @@
-
+# This took me so long because I accidentally had "if inp[i][j] == '#'" in the nested loop check
+# Didn't spot it, cost me ~25 minutes or 3 places on the lb
 with open("input.txt", "r") as f:
     inp = [line.strip() for line in f.readlines()]
+
+
+rows = []
+for i, l in enumerate(inp):
+    if '#' not in l:
+        rows.append(i)
+cols = []
+for i in range(len(inp[0])):
+    empty = True
+    for j in range(len(inp)):
+        empty = inp[j][i] != '#'
+        if not empty:
+            break
+    if empty:
+        cols.append(i)
+
+part1 = 0
+part2 = 0
+for i in range(len(inp)):
+    for j in range(len(inp[0])):
+        if inp[i][j] == '#':
+            for k in range(len(inp)):
+                for l in range(len(inp[0])):
+                    if inp[k][l] == '#':
+                        part1 += abs((k - i)) + abs((l - j)) + sum(1 for x in cols if l < x < j or j < x < l) + sum(1 for x in rows if k < x < i or i < x < k)
+                        part2 += abs((k - i)) + abs((l - j)) + sum(999999 for x in cols if l < x < j or j < x < l) + sum(999999 for x in rows if k < x < i or i < x < k)
+print(f"Part 1: {part1//2}")
+print(f"Part 2: {part2//2}")
