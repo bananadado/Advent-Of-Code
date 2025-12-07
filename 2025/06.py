35da396e31ab42bcd560dcaacb96bde61da1fbce
Leo Matteucci <leo.matteucci@gmail.com>
2025-12-07 13:05:20 +0000

    062025 - looping

--- 2025/06.py
+++ 2025/06.py
@@ -3,16 +3,16 @@ from itertools import groupby
 from math import prod
 with open("input.txt","r") as f:
    inp = [line.strip("\n") for line in f.readlines()]
-   data = inp[:-1]
+   data, ops = inp[:-1], inp[-1].split()
    # rotation trick from many moons ago of unzipping then zipping
    nums = list(zip(*(map(int, l.split()) for l in data)))
    nums2 = [list(map(int, g)) for k, g in groupby(("".join(l).strip() for l in zip(*data)), bool) if k]
-   ops = inp[-1].split()
 
 p1, p2 = 0, 0
-for i in range(len(ops)):
-    p1 += prod(nums[i]) if ops[i] == "*" else sum(nums[i])
-    p2 += prod(nums2[i]) if ops[i] == "*" else sum(nums2[i])
+for op, n1, n2 in zip(ops, nums, nums2):
+    f = prod if op == "*" else sum
+    p1 += f(n1)
+    p2 += f(n2)
 
 print(f"Part 1: {p1}")
 print(f"Part 2: {p2}")
